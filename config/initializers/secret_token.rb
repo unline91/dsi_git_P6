b8b389d25e5667f3fb3b4b78af74b835c4de8716
Carlos <carlos@dsi.(none)>
2013-04-02 12:26:30 +0100

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DsiGit4::Application.config.secret_token = '2e9f7f5600131855bf87a9804fa7b8010ffb938ae6412cfaa2d7f817fb181695c3f3a31f722623cc9872c63b30060b4764bea3b87baf66a79c71e4a664ecdf3a'
